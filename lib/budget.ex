e4238a23b3cee77c98c91301e325fd0f59cb1674
sege <sebastianLobato.genco@tomtom.com>
2017-03-24 16:30:07 +0100

    Initial commit with basic Elixir app setup

---
+++ lib/budget.ex
@@ -0,0 +1,31 @@
+defmodule Budget do
+  @moduledoc """
+  Documentation for Budget.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Budget.hello
+      :world
+
+  """
+  def hello(var, dos) do
+    IO.puts "#{var}-#{dos}"
+  end
+
+  def list_transactions do
+    File.read!("transactions-jan.csv")
+    |> parse
+    # |> filter
+    # |> normalize
+    # |> sort
+    # |> print
+  end
+
+  def parse(string) do
+
+  end
+end
